16c52708930385a4e74e1974d1a9fad6a6c34f28
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-13 16:03:42 -0400

    Added letter range to board

--- gameboard.py
+++ gameboard.py
@@ -7,16 +7,18 @@ class Gameboard:
     def make_gameboard(self):
         cols = 21
         rows = 21
+        alpha_list = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' ,'j', 
+                        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't']
         # self.gameboard = [[0] * cols] * rows
         self.gameboard = [[0 for i in range(cols)] for j in range(rows)]
         i = 0
         while i < 21:
             self.gameboard[i][0] = i
             i += 1
-        self.gameboard = [[0 for i in range(cols)] for j in range(rows)]
+        # self.gameboard = [[0 for i in range(cols)] for j in range(rows)]
         i = 0
         while i < 21:
-            self.gameboard[0][i] = i
+            self.gameboard[0][i] = alpha_list[i]
             i += 1
         
 
