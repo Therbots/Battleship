4e9014d22ce948286a5756583fc64a85aa79e6c8
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-13 15:11:24 -0400

    Made game pieces destroyer, submarine, battleship, and aircraft carrier

--- game_pieces.py
+++ game_pieces.py
@@ -1,3 +1,15 @@
 class Game_Pieces:
     def __init__(self):
         pass
+
+    def destroyer(self):
+        pass
+
+    def submarine(self):
+        pass
+
+    def battleship(self):
+        pass
+    
+    def aircraft_carrier(self):
+        pass
