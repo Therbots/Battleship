f8bd7e035d8e6951d74d0985fbb7acdb160de121
taylor herbots <therbots@live.com>
2021-08-13 14:51:45 -0500

    added cooridinates to our gameboard in gameboard class

--- gameboard.py
+++ gameboard.py
@@ -5,10 +5,20 @@ class Gameboard:
 
 
     def make_gameboard(self):
-        cols = 20
-        rows = 20
-        self.gameboard = [[0] * cols] * rows
-        self.gameboard[0][0] = 1
+        cols = 21
+        rows = 21
+        # self.gameboard = [[0] * cols] * rows
+        self.gameboard = [[0 for i in range(cols)] for j in range(rows)]
+        i = 0
+        while i < 21:
+            self.gameboard[i][0] = i
+            i += 1
+        self.gameboard = [[0 for i in range(cols)] for j in range(rows)]
+        i = 0
+        while i < 21:
+            self.gameboard[0][i] = i
+            i += 1
+        
 
         for row in self.gameboard:
             print(row)
