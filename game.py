4af5fc1d8c1bff6fd0defc5da52419ad98797c31
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-16 12:05:54 -0400

    added make_gameboards method

--- game.py
+++ game.py
@@ -5,6 +5,9 @@ from gameboard import Gameboard
 
 class Game:
     def __init__(self):
+        self.make_gameboards()
+
+    def make_gameboards(self):
         gameboard_p1_self = Gameboard()
         gameboard_p1_opp = Gameboard()
         gameboard_p2_self = Gameboard()
@@ -13,6 +16,7 @@ class Game:
         gameboard_p1_opp.make_gameboard()
         gameboard_p2_self.make_gameboard()
         gameboard_p2_opp.make_gameboard()
+
 
     def run_game(self):
         pass
