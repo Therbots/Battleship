aad96bbb3390f9ccb65bea4e2245425b11a69145
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-16 15:26:11 -0400

    Made player 2 turn

--- game.py
+++ game.py
@@ -12,7 +12,28 @@ class Game:
     def run_game(self):
         self.player_one.set_pieces()
         self.player_two.set_pieces()
-       
+
+    def game_turn(self):
+
+
+
+        # while(gameover):
+            #Player 1 turn
+            player1_turn_row =input("Player 1 enter row for attack: ")
+            player1_turn_col = input("Player 1 enter column for attack: ")
+            if(self.player_two.gameboard_self[player1_turn_row][player1_turn_col] == "o"):
+                self.player_one.gameboard_opp[player1_turn_row][player1_turn_col] = "x"
+                self.player_two.gameboard_self[player1_turn_row][player1_turn_col] = "x"
+            
+            #Player 2 turn
+            player2_turn_row =input("Player 2 enter row for attack: ")
+            player2_turn_col = input("Player 2 enter column for attack: ")
+            if(self.player_one.gameboard_self[player2_turn_row][player2_turn_col] == "o"):
+                self.player_two.gameboard_opp[player2_turn_row][player2_turn_col] = "x"
+                self.player_one.gameboard_self[player2_turn_row][player2_turn_col] = "x"
+
+
+
 
         
         
